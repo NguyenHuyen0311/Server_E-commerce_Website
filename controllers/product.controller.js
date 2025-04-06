5de6dc480e2355165cc4ef2a714aba68c16755b4
NguyenHuyen0311 <huyenmeroria@gmail.com>
2025-04-06 19:27:38 +0700

    Delete Image, Get - Delete - Update Product

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -411,32 +411,336 @@ export async function getAllProductsByThirdCatName(req, res) {
   }
 }
 
-// // Create product
-// export async function createProduct(req, res) {
-//   try {
-//   } catch (error) {
-//     return res
-//       .status(500)
-//       .json({ message: error.message || error, error: true, success: false });
-//   }
-// }
+// Get all products by price
+export async function getAllProductsByPrice(req, res) {
+  try {
+    let productList = [];
 
-// // Create product
-// export async function createProduct(req, res) {
-//   try {
-//   } catch (error) {
-//     return res
-//       .status(500)
-//       .json({ message: error.message || error, error: true, success: false });
-//   }
-// }
+    if (req.query.catId !== "" && req.query.catId !== undefined) {
+      const productListArr = await ProductModel.find({
+        catId: req.query.catId,
+      }).populate("category");
 
-// // Create product
-// export async function createProduct(req, res) {
-//   try {
-//   } catch (error) {
-//     return res
-//       .status(500)
-//       .json({ message: error.message || error, error: true, success: false });
-//   }
-// }
+      productList = productListArr;
+    }
+
+    if (req.query.subCatId !== "" && req.query.subCatId !== undefined) {
+      const productListArr = await ProductModel.find({
+        subCatId: req.query.subCatId,
+      }).populate("category");
+
+      productList = productListArr;
+    }
+
+    if (
+      req.query.thirdSubCatId !== "" &&
+      req.query.thirdSubCatId !== undefined
+    ) {
+      const productListArr = await ProductModel.find({
+        thirdSubCatId: req.query.thirdSubCatId,
+      }).populate("category");
+
+      productList = productListArr;
+    }
+
+    const filteredProducts = productList.filter((product) => {
+      if (req.query.minPrice && product.price < parseInt(+req.query.minPrice)) {
+        return false;
+      }
+      if (req.query.maxPrice && product.price > parseInt(+req.query.maxPrice)) {
+        return false;
+      }
+      return true;
+    });
+
+    return res.status(200).json({
+      error: false,
+      success: true,
+      products: filteredProducts,
+      totalPages: 0,
+      page: 0,
+    });
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ message: error.message || error, error: true, success: false });
+  }
+}
+
+// Get all products by rating
+export async function getAllProductsByRating(req, res) {
+  try {
+    const page = parseInt(req.body.page) || 1;
+    const perPage = parseInt(req.query.perPage) || 10000;
+    const totalPosts = await ProductModel.countDocuments();
+    const totalPages = Math.ceil(totalPosts / perPage);
+
+    if (page > totalPages) {
+      return res.status(404).json({
+        message: "Page not found",
+        error: true,
+        success: false,
+      });
+    }
+
+    let products = [];
+
+    if (req.query.catId !== undefined) {
+      products = await ProductModel.find({
+        rating: req.query.rating,
+        catId: req.query.catId,
+      })
+        .populate("category")
+        .skip((page - 1) * perPage)
+        .limit(perPage)
+        .exec();
+    }
+
+    if (req.query.subCatId !== undefined) {
+      products = await ProductModel.find({
+        rating: req.query.rating,
+        subCatId: req.query.subCatId,
+      })
+        .populate("category")
+        .skip((page - 1) * perPage)
+        .limit(perPage)
+        .exec();
+    }
+
+    if (req.query.thirdSubCatId !== undefined) {
+      products = await ProductModel.find({
+        rating: req.query.rating,
+        thirdSubCatId: req.query.thirdSubCatId,
+      })
+        .populate("category")
+        .skip((page - 1) * perPage)
+        .limit(perPage)
+        .exec();
+    }
+
+    if (!products) {
+      res.status(500).json({
+        error: true,
+        success: false,
+      });
+    }
+
+    return res.status(200).json({
+      error: false,
+      success: true,
+      products: products,
+      totalPages: totalPages,
+      page: page,
+    });
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ message: error.message || error, error: true, success: false });
+  }
+}
+
+// Get all products count
+export async function getAllProductsCount(req, res) {
+  try {
+    const productsCount = await ProductModel.countDocuments();
+
+    if (!productsCount) {
+      res.status(500).json({
+        error: true,
+        success: false,
+      });
+    }
+
+    return res.status(500).json({
+      error: false,
+      success: true,
+      productsCount: productsCount,
+    });
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ message: error.message || error, error: true, success: false });
+  }
+}
+
+// Get all features products
+export async function getAllFeaturedProducts(req, res) {
+  try {
+    const products = await ProductModel.find({
+      isFeatured: true,
+    }).populate("category");
+
+    if (!products) {
+      res.status(500).json({
+        error: true,
+        success: false,
+      });
+    }
+
+    return res.status(200).json({
+      error: false,
+      success: true,
+      products: products,
+    });
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ message: error.message || error, error: true, success: false });
+  }
+}
+
+// Delete product
+export async function deleteProduct(req, res) {
+  try {
+    const product = await ProductModel.findById(req.params.id).populate(
+      "category"
+    );
+
+    if (!product) {
+      return res.status(404).json({
+        message: "Product not found",
+        error: true,
+        success: false,
+      });
+    }
+
+    const images = product.images;
+
+    for (img of images) {
+      const imgUrl = img;
+      const urlArr = imgUrl.split("/");
+      const image = urlArr[urlArr.length - 1];
+
+      const imageName = image.split(".")[0];
+
+      if (imageName) {
+        cloudinary.uploader.destroy(imageName, (error, result) => {
+          console.log(error, result);
+        });
+      }
+    }
+
+    const deleteProduct = await ProductModel.findByIdAndDelete(req.params.id);
+
+    if (!deleteProduct) {
+      return res.status(404).json({
+        message: "Product not deleted",
+        error: true,
+        success: false,
+      });
+    }
+
+    return res.status(200).json({
+      message: "Product deleted!",
+      error: false,
+      success: true,
+    });
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ message: error.message || error, error: true, success: false });
+  }
+}
+
+// Get single product
+export async function getProduct(req, res) {
+  try {
+    const product = await ProductModel.findById(req.params.id).populate(
+      "category"
+    );
+
+    if (!product) {
+      return res.status(404).json({
+        message: "Product not found",
+        error: true,
+        success: false,
+      });
+    }
+
+    return res.status(404).json({
+      error: false,
+      success: true,
+      product: product,
+    });
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ message: error.message || error, error: true, success: false });
+  }
+}
+
+// Remove image from Cloudinary
+export async function removeImageFromCloudinary(req, res) {
+  const imgUrl = req.query.img;
+
+  const urlArr = imgUrl.split("/");
+  const image = urlArr[urlArr.length - 1];
+
+  const imageName = image.split(".")[0];
+
+  if (imageName) {
+    const result = await cloudinary.uploader.destroy(
+      imageName,
+      (error, result) => {
+        console.log("Image deleted:", result, error);
+      }
+    );
+
+    if (result) {
+      res.status(200).send(result);
+    }
+  }
+}
+
+// Update product
+export async function updateProduct(req, res) {
+  try {
+    const product = await ProductModel.findByIdAndUpdate(
+      req.params.id,
+      {
+        name: req.body.name,
+        description: req.body.description,
+        images: req.body.images,
+        brand: req.body.brand,
+        price: req.body.price,
+        oldPrice: req.body.oldPrice,
+        catName: req.body.catName,
+        catId: req.body.catId,
+        subCatId: req.body.subCatId,
+        subCatName: req.body.subCatName,
+        thirdSubCatId: req.body.thirdSubCatId,
+        thirdSubCatName: req.body.thirdSubCatName,
+        category: req.body.category,
+        countInStock: req.body.countInStock,
+        rating: req.body.rating,
+        isFeatured: req.body.isFeatured,
+        discount: req.body.discount,
+        productWeight: req.body.productWeight,
+        productFlavor: req.body.productFlavor,
+      },
+      {
+        new: true,
+      }
+    );
+
+    if (!product) {
+      return res.status(404).json({
+        message: "Product cannot be updated!",
+        error: true,
+        success: false,
+      });
+    }
+
+    imagesArr = [];
+
+    return res.status(200).json({
+      message: "The product is updated!",
+      error: false,
+      success: true,
+    });
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ message: error.message || error, error: true, success: false });
+  }
+}
--- routes/product.route.js
+++ routes/product.route.js
@@ -1,7 +1,7 @@
 import { Router } from 'express';
 import auth from '../middlewares/auth.js';
 import upload from '../middlewares/multer.js';
-import { createProduct, getAllProducts, getAllProductsByCatId, getAllProductsByCatName, getAllProductsBySubCatId, getAllProductsBySubCatName, getAllProductsByThirdCatId, getAllProductsByThirdCatName, uploadImages } from '../controllers/product.controller.js';
+import { createProduct, deleteProduct, getAllFeaturedProducts, getAllProducts, getAllProductsByCatId, getAllProductsByCatName, getAllProductsByPrice, getAllProductsByRating, getAllProductsBySubCatId, getAllProductsBySubCatName, getAllProductsByThirdCatId, getAllProductsByThirdCatName, getAllProductsCount, getProduct, removeImageFromCloudinary, updateProduct, uploadImages } from '../controllers/product.controller.js';
 
 const productRouter = Router();
 productRouter.post('/upload-images', auth, upload.array('images'), uploadImages);
@@ -13,5 +13,13 @@ productRouter.get('/getAllProductsBySubCatId/:id', getAllProductsBySubCatId);
 productRouter.get('/getAllProductsBySubCatName', getAllProductsBySubCatName);
 productRouter.get('/getAllProductsByThirdCatId/:id', getAllProductsByThirdCatId);
 productRouter.get('/getAllProductsByThirdCatName', getAllProductsByThirdCatName);
+productRouter.get('/getAllProductsByPrice', getAllProductsByPrice);
+productRouter.get('/getAllProductsByRating', getAllProductsByRating);
+productRouter.get('/getAllProductsCount', getAllProductsCount);
+productRouter.get('/getAllFeaturedProducts', getAllFeaturedProducts);
+productRouter.delete('/:id', deleteProduct);
+productRouter.get('/:id', getProduct);
+productRouter.delete('/delete-image', auth, removeImageFromCloudinary);
+productRouter.put('/updateProduct/:id', auth, updateProduct);
 
 export default productRouter;
